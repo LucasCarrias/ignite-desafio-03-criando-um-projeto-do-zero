46572c3bcc591630ee69821146143b5c5f300ee5
Lucas Carrias <lucascarrias@outlook.com>
2021-06-07 21:48:38 -0300

    Create header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,24 @@
-export default function Header() {
-  // TODO
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  const router = useRouter();
+
+  function handleClick(): void {
+    router.push('');
+  }
+
+  return (
+    <header className={styles.header}>
+      <Image
+        src="/logo.svg"
+        alt="logo"
+        width="240"
+        height="25"
+        onClick={handleClick}
+      />
+    </header>
+  );
 }
